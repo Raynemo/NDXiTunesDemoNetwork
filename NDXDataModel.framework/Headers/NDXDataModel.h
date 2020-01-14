8ccee9ebfbe6cd47bf7f05b099ac0c276992ca0a
Raynemo <rajan.ayyappan@kiswe.com>
2020-01-14 09:50:21 -0800

    added datamodel framework

---
+++ NDXDataModel.framework/Headers/NDXDataModel.h
@@ -0,0 +1,19 @@
+//
+//  NDXDataModel.h
+//  NDXDataModel
+//
+//  Created by RajanAR21 on 1/6/20.
+//  Copyright © 2020 RajanAR21. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NDXDataModel.
+FOUNDATION_EXPORT double NDXDataModelVersionNumber;
+
+//! Project version string for NDXDataModel.
+FOUNDATION_EXPORT const unsigned char NDXDataModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NDXDataModel/PublicHeader.h>
+
+
